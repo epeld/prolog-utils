d97f2f985635faf96d50fe734673351112685dc8
Erik Peldan <erik.peldan@gmail.com>
2018-06-02 15:31:41 +0200

    Work on a test app in pdffile

--- pdffile.pl
+++ pdffile.pl
@@ -1,8 +1,54 @@
 :- module(pdffile, []).
 :- use_module(library(clpfd)).
+:- use_module(pdf).
 
 :- set_prolog_flag(double_quotes, codes).
+:- set_prolog_flag(backtrace_depth, 40).
+:- set_prolog_flag(backtrace_goal_depth, 5).
+:- set_prolog_flag(debugger_write_options,
+                   [quoted(true), portray(true), max_depth(30), priority(699)]).
 
+test_app :-
+  parse_file("/home/erik/Downloads/halmos.pdf", _Ctx).
+
+parse_file(FileName, Context) :-
+  setup_call_cleanup(
+    open(FileName, read, Stream, [type(binary)]),
+    (
+      context_from_stream(Stream, Context),
+      do_something(Context)
+    ),
+    close(Stream)
+  ).
+
+%
+% Construct a pdf-context from the given stream.
+% The context will give you access to the rest of the API
+%
+context_from_stream(Stream, context(Stream, XRef, Trailer)) :-
+  locate_xref(Stream, _Offset),
+  phrase_from_stream(
+    (
+      pdf:xref(XRef),
+      pdf:trailer(Trailer)
+    ),
+    Stream
+  ),
+  !.
+
+
+do_something(Context) :-
+  forall(
+    context_reify_object(Context, Ref, Object),
+    (
+      pdf:all_object_references(Object, Refs),
+      format("Object ~w -> ~w~n", [Ref, Refs])
+    )
+  ).
+
+%
+% Seek to start of X-ref section
+%
 locate_xref(Stream, Offset) :-
   seek(Stream, -100, eof, _Pos),
   phrase_from_stream(
@@ -15,35 +61,23 @@ locate_xref(Stream, Offset) :-
   seek(Stream, Offset, bof, Offset),
   !.
 
-
-parse_file(FileName, Context) :-
-  setup_call_cleanup(
-    open(FileName, read, Stream, [type(binary)]),
-    (
-      parse_stream(Stream, Context),
-      do_something(Context)
-    ),
-    close(Stream)
-  ).
-
-parse_stream(Stream, context(Stream, XRef, Trailer)) :-
-  locate_xref(Stream, _Offset),
-  phrase_from_stream((
-                        pdf:xref(XRef),
-                        pdf:trailer(Trailer)
-                      ),
-                     Stream),
-  !.
-
-
-do_something(Context) :-
-  context_reify_object(Context, reference(132, 0), Object),
-  format("Found object?! ~w~n", [Object]).
-
+%
+% Reify = locate + Read
+%
 context_reify_object(Context, Reference, Object) :-
   context_locate_object(Context, Reference),
-  context_read_object(Context, Object).
+  format("Located ~w", [Reference]),
+  (
+    context_read_object(Context, Object)
+    *->
+    format(" - OK~n", []) ;
+    format("FAIL ~n")
+  )
+.
 
+%
+% Read == Parse out object from pdffile
+%
 context_read_object(Context, Object) :-
   context_stream(Context, Stream),
   phrase_from_stream(
@@ -54,12 +88,18 @@ context_read_object(Context, Object) :-
     Stream),
   !.
 
+%
+% Locate = Look up object in X-ref, seek to offset
+%
 context_locate_object(Context, Reference) :-
   context_xref(Context, XRef),
   context_stream(Context, Stream),
-  pdf:xref_lookup(XRef, Reference, Offset),
   !,
+  pdf:xref_lookup(XRef, Reference, Offset),
   seek(Stream, Offset, bof, Offset).
 
+%
+% Context getters
+%
 context_stream(context(S,_1,_2), S).
 context_xref(context(_1,X,_2), X).
