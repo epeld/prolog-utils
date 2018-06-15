7ba70ef03a357a84a8ea0091874b1c9698c2e066
Erik Peldan <erik.peldan@gmail.com>
2018-06-15 17:58:43 +0200

    WIP code_app

---
+++ code_app.pl
@@ -0,0 +1,13 @@
+%
+% This is a test app to try and parse som PDF-code
+%
+:- module(code_app, []).
+:- use_module(pdffile).
+
+:- set_prolog_flag(double_quotes, codes).
+:- set_prolog_flag(backtrace_depth, 40).
+:- set_prolog_flag(backtrace_goal_depth, 5).
+:- set_prolog_flag(debugger_write_options,
+                   [quoted(true), portray(true), max_depth(30), priority(699)]).
+
+
