735e0e0531d2b113c02f052c9f35e1d61f36cc96
Erik Peldan <erik.peldan@gmail.com>
2018-06-16 12:52:47 +0200

    Parse pdf commands

--- code.pl
+++ code.pl
@@ -43,15 +43,34 @@ argument(Arg) -->
 
 arguments([Arg | Args]) -->
   argument(Arg),
-  arguments_(Args).
+  arguments_(Args, Arg).
 
-arguments_([Arg | Args]) -->
-  pdf:optional_space,
+arguments_([Arg | Args], Prev) -->
+  special_whitespace(Prev),
   argument(Arg),
-  arguments_(Args).
+  arguments_(Args, Arg).
 
-arguments_([]) --> [].
+arguments_([], _) --> [].
 
+special_whitespace(N), [Digit] -->
+  {
+    number(N) ; key(N)
+  },
+  pdf:whitespace,
+  pdf:digit(Digit).
+
+special_whitespace(Other) -->
+  {
+    not_number(Other)
+  },
+  pdf:optional_space.
+
+key(key(_Codes)).
+
+not_number(Other) :-
+  \+ var(Other),
+  \+ number(Other),
+  \+ key(Other).
 
 :- begin_tests(code).
 
@@ -69,6 +88,9 @@ test(command_name3) :-
   phrase(command_name(C), "Td"),
   C = td.
 
+test(arguments0, all(C = [[211.283]])) :-
+  phrase(arguments(C), "211.283").
+
 
 test(arguments, nondet) :-
   phrase(arguments(C), "211.283 683.997"),
@@ -77,5 +99,8 @@ test(arguments, nondet) :-
 test(command, nondet) :-
   phrase(code_command(C), "211.283 683.997 Td"),
   C = td(211.283, 683.997).
+
+test(command2, all(C = [tf(key("F30"), 9.963)])) :-
+  phrase(code_command(C), "/F30 9.963 Tf").
 
 :- end_tests(code).
