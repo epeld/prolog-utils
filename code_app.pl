dbb12ed63e6b9682fe8c5fbe5433b48b2076c341
Erik Peldan <erik.peldan@gmail.com>
2018-06-16 10:39:16 +0200

    Implement POC-app for extracting PS-code

--- code_app.pl
+++ code_app.pl
@@ -19,5 +19,78 @@ do_something(Context) :-
   format("Program started~n"),
   pdffile:context_reify_object(Context, Reference, Object),
   pdf:object_type(Object, stream),
+
+  % Print info
   !,
-  format("Object ~w has type ~w~n", [Reference, stream]).
+  format("Object ~w has type ~w~n", [Reference, stream]),
+  pretty_print_object(Object),
+
+  % Locate data
+  pdffile:context_stream(Context, Stream),
+  pdffile:context_locate_object(Context, Reference),
+  find_stream_data_offset(Stream, Object, Offset),
+
+  % Open stream
+  !,
+  seek(Stream, Offset, bof, Offset),
+  setup_call_cleanup(
+    zopen(
+      Stream, ZStream,
+      [
+        close_parent(false)
+      ]
+    ),
+    phrase_from_stream(everything(Content), ZStream),
+    close(ZStream)
+  ),
+
+  !,
+  format("---PDF CODE---~n"),
+  format(Content),
+  format("--------------~n").
+
+
+find_stream_data_offset(Stream, Object, Offset) :-
+  phrase_from_stream(
+    (
+      pdf:stream_object_header(Object),
+      lazy_list_character_count(Offset),
+      pdf:gibberish2
+    ),
+    Stream
+  ).
+
+everything([A | Rest]) --> [A], everything(Rest).
+everything([]) --> [].
+
+abc(Content, Length) -->
+  {
+    length(Content, Length)
+  },
+  Content, pdf:gibberish2.
+
+abc2(Content) -->
+  lazy_list_character_count(Content),
+  pdf:gibberish2.
+
+pretty_print_object(Object) :-
+  pdf:object_dictionary(Object, Dictionary),
+  pretty_print_dictionary(Dictionary).
+
+pretty_print_dictionary(D) :-
+  format("<<~n"),
+  forall(
+    member(K-V, D),
+    (
+      key_string(K, KS),
+      key_string(V, VS),
+      format("~w = ~w~n", [KS, VS])
+    )
+  ),
+  format(">>~n").
+
+key_string(key(Codes), String) :-
+  string_codes(String, Codes).
+
+key_string(Other, Other) :-
+  Other \= key(_).
