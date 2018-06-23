6802ae3c78e4a27e72911dacfe0b5c4be1331425
Erik Peldan <erik.peldan@gmail.com>
2018-06-23 20:06:11 +0200

    Problem: cannot pass parameters easily to main Solution: implement basic command parsing

--- main.pl
+++ main.pl
@@ -2,6 +2,54 @@
 % :- use_module(main, []).
 
 main :-
-  FileName = "/home/erik/Downloads/halmos.pdf",
-  Reference = reference(3, 0),
-  code_app:run(print, FileName, Reference).
+  argv(Args),
+  main_with_args(Args).
+
+main_with_args(Args) :-
+  args_filename(Args, FileName, Rest0),
+  args_reference(Rest0, Reference, Rest1),
+  args_empty(Rest1),
+  run(FileName, Reference).
+
+
+run(FileName, Reference) :-
+  catch(
+    code_app:run(print, FileName, Reference),
+    Error,
+    (
+      format_error("Error: ~w~n", [Error]),
+      true
+    )
+  ).
+
+
+
+args_filename([FileName | Rest], FileName, Rest) :- !.
+args_filename(_A, _B, _C) :-
+  print_usage, fail.
+
+
+args_reference([ReferenceString | Rest], Reference, Rest) :-
+  !,
+  (
+    pdf:parse_reference(ReferenceString, Reference)
+    *-> !
+  ; ( format_error("Cannot parse ~w~n", [ReferenceString]), fail )
+  ).
+args_reference(_A, _B, _C) :-
+  print_usage, fail.
+
+
+args_empty([]) :- !.
+args_empty(_A) :-
+  print_usage, fail.
+
+
+print_usage :-
+  format_error("Usage: prologpdf <FileName> [<Reference>]~n", []).
+
+
+format_error(Message, Args) :-
+  stream_property(Stream, alias(user_error)),
+  !,
+  format(Stream, Message, Args).
