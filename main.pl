72e8fca89543ca192d3defb3cb8feffd7de0ad82
Erik Peldan <erik.peldan@gmail.com>
2018-06-23 21:02:05 +0200

    Problem: We don't know how to debug objects that are slow to parse Solution: Define a 'raw'-mode for printing raw object chars

--- main.pl
+++ main.pl
@@ -15,7 +15,7 @@ mode_exists(Mode) :-
 
 run(print, [FileName, Reference]) :-
   once(
-    parse_reference(Reference, R)
+    pdf:parse_reference(Reference, R)
   ),
   print(FileName, R).
 
@@ -23,6 +23,28 @@ run(list, [FileName]) :-
   pdffile:with_file_context(FileName,
                             main:list_objects).
 
+
+run(raw, [FileName, Reference]) :-
+  once(
+    pdf:parse_reference(Reference, R)
+  ),
+  pdffile:with_file_context(FileName,
+                            main:print_raw_object(R)).
+
+
+print_raw_object(Reference, Context) :-
+  pdffile:context_locate_object(Context, Reference),
+  pdffile:context_read_object(Context, _Object, Start, Stop),
+  pdffile:context_locate_object(Context, Reference),
+  !,
+  Length is Stop - Start,
+  length(Codes, Length),
+  pdffile:context_stream(Context, Stream),
+  phrase_from_stream(
+    (Codes, pdf:gibberish2),
+    Stream
+  ),
+  format(Codes).
 
 list_objects(Context) :-
   forall(
