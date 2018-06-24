49fb39d35781f67c31ce2503512733998f053163
Erik Peldan <erik.peldan@gmail.com>
2018-06-24 21:18:10 +0200

    Problem: don't know how to transform pdf-code to text Solution: make a stab at it

--- interpreter.pl
+++ interpreter.pl
@@ -3,19 +3,75 @@
 %
 :- module(interpreter, []).
 
-interpret(Commands) :-
+interpret(Commands, Stream) :-
   initial_state(State0),
+  !,
   interpret_(Commands, State0, State1),
+  !,
+  format("Done interpreting~n"),
   reverse(State1, State2),
-  maplist(atomify, State2, State),
-  format("~w~n", [State]).
+  remove_header(State2, State3),
+  detect_new_paragaph(State3, State4_0),
+  join_strings(State4_0, State4),
+  maplist(atomify, State4, State),
+  format(Stream, "~w~n", [State]).
+
+join_strings([string(A), string(B) | Rest0], Rest) :-
+  !,
+  append(A,B,AB),
+  join_strings([string(AB) | Rest0], Rest).
+
+join_strings([A | Rest0], [A | Rest]) :-
+  join_strings(Rest0, Rest).
+
+join_strings([], []).
+
+remove_header([paragraph(_A, Size, _C), string(_Blabla) | State], State) :-
+  very_close(Size, 6.974).
+
+detect_new_paragaph([move(X, Y) | Rest0], Rest) :-
+  very_close(X, 11.956),
+  very_close(Y, -11.956),
+  !,
+  detect_new_paragaph([new_paragraph | Rest0], Rest).
+
+detect_new_paragaph([new_paragraph, string(Something), move(X, Y) | Rest0],
+                    [new_paragraph, string(Something) | Rest]) :-
+  very_close(X, -11.956),
+  very_close(Y, -11.956),
+  !,
+  detect_new_paragaph(Rest0, Rest).
+
+detect_new_paragaph([move(0, Y) | Rest0],
+                    Rest) :-
+  very_close(Y, -11.956),
+  !,
+  detect_new_paragaph(Rest0, Rest).
+
+
+detect_new_paragaph([A | Rest0], [A | Rest]) :-
+  detect_new_paragaph(Rest0, Rest).
+
+detect_new_paragaph([], []).
+
+
+very_close(A, B) :-
+  Diff is A - B,
+  Diff >= 0,
+  Diff < 0.1.
+
+very_close_abs(A, B) :-
+  very_close(A, B) ; very_close(B, A).
+
 
 atomify(move(A,B), move(A,B)) :- !.
 
-atomify(string(Codes), String) :- !, format(string(String), Codes, []).
+atomify(string(Codes), string(String)) :- !, format(string(String), Codes, []).
 
 atomify(paragraph(key(Codes), S, _1), paragraph(String, S)) :- !, format(string(String), Codes, []).
 
+
+atomify(A, A) :- !.
 atomify(A, Name) :-
   functor(A, Name, _).
 
