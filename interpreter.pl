0cb214cd1ed7b925c539897659cdc859142580ce
Erik Peldan <erik.peldan@gmail.com>
2018-06-25 20:03:33 +0200

    Problem: interpreter-module does too much

--- interpreter.pl
+++ interpreter.pl
@@ -2,6 +2,7 @@
 % This module tries to interpret pdf ps code
 %
 :- module(interpreter, []).
+:- use_module(transform, [transform/2]).
 
 interpret(Commands, Stream) :-
   initial_state(State0),
@@ -9,69 +10,11 @@ interpret(Commands, Stream) :-
   interpret_(Commands, State0, State1),
   !,
   format("Done interpreting~n"),
-  reverse(State1, State2),
-  remove_header(State2, State3),
-  detect_new_paragaph(State3, State4_0),
-  join_strings(State4_0, State4),
-  maplist(atomify, State4, State),
-  format(Stream, "~w~n", [State]).
+  transform(State1, State),
+  format("Writing to file~n"),
+  format(Stream, "~w~n", [State]),
+  format("Done~n").
 
-join_strings([string(A), string(B) | Rest0], Rest) :-
-  !,
-  append(A,B,AB),
-  join_strings([string(AB) | Rest0], Rest).
-
-join_strings([A | Rest0], [A | Rest]) :-
-  join_strings(Rest0, Rest).
-
-join_strings([], []).
-
-remove_header([font(_A, Size, _C), string(_Blabla) | State], State) :-
-  very_close(Size, 6.974).
-
-detect_new_paragaph([move(X, Y) | Rest0], Rest) :-
-  very_close(X, 11.956),
-  very_close(Y, -11.956),
-  !,
-  detect_new_paragaph([new_paragraph | Rest0], Rest).
-
-detect_new_paragaph([new_paragraph, string(Something), move(X, Y) | Rest0],
-                    [new_paragraph, string(Something) | Rest]) :-
-  very_close(X, -11.956),
-  very_close(Y, -11.956),
-  !,
-  detect_new_paragaph(Rest0, Rest).
-
-detect_new_paragaph([move(0, Y) | Rest0],
-                    Rest) :-
-  very_close(Y, -11.956),
-  !,
-  detect_new_paragaph(Rest0, Rest).
-
-
-detect_new_paragaph([A | Rest0], [A | Rest]) :-
-  detect_new_paragaph(Rest0, Rest).
-
-detect_new_paragaph([], []).
-
-
-very_close(A, B) :-
-  Diff is A - B,
-  Diff >= 0,
-  Diff < 0.1.
-
-very_close_abs(A, B) :-
-  very_close(A, B) ; very_close(B, A).
-
-
-atomify(move(A,B), move(A,B)) :- !.
-
-atomify(string(Codes), string(String)) :- !, format(string(String), Codes, []).
-
-
-atomify(A, A) :- !.
-atomify(A, Name) :-
-  functor(A, Name, _).
 
 
 initial_state([]).
